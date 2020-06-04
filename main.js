1de05369d2fbfa168fa5895fc10a4b949048aa2a
Sayan Mondal <sayan.mondal.tech@gmail.com>
2020-06-04 20:27:34 +0530

    variables created

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("test check !!!");
+let min = 1,
+  max = 10,
+  winingNumber = 7,
+  guessLeft = 3;
+
+const minInput = document.querySelector(".min-class"),
+  game = document.querySelector("#game"),
+  maxInput = document.querySelector(".max-class"),
+  guessInput = document.querySelector(".guess-btn"),
+  mssg = document.querySelector(".message");
