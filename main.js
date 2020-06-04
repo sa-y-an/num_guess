f63b6adecf1058400c1ce81d6573cbe02f2e3733
Sayan Mondal <sayan.mondal.tech@gmail.com>
2020-06-04 20:56:07 +0530

    js added

--- main.js
+++ main.js
@@ -6,5 +6,23 @@ let min = 1,
 const minInput = document.querySelector(".min-class"),
   game = document.querySelector("#game"),
   maxInput = document.querySelector(".max-class"),
-  guessInput = document.querySelector(".guess-btn"),
+  guessButton = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
   mssg = document.querySelector(".message");
+
+minInput.textContent = min;
+maxInput.textContent = max;
+
+guessButton.addEventListener("click", function () {
+  let guessNumber = parseInt(guessInput.value);
+
+  if (isNaN(guessNumber) || guessNumber < 1 || guessNumber > 10) {
+    message(`please enter a valid number between ${min} and ${max}`, "red");
+  } else {
+  }
+});
+
+function message(msg, color) {
+  mssg.textContent = msg;
+  mssg.style.color = color;
+}
